3ea44abadd20ae64e4a1a8355c004f50336a3bd2
Mickey Barboi <damouse007@gmail.com>
2015-11-08 16:16:41 -0600

    removed the views file

--- ExAgainst/GameViewController.swift
+++ ExAgainst/GameViewController.swift
@@ -33,9 +33,6 @@ class GameViewController: UIViewController {
     @IBOutlet weak var collectionPlayers: UICollectionView!
     @IBOutlet weak var buttonBack: UIButton!
     
-    var tableDelegate: CardTableDelegate?
-    var collectionDelegate: PlayerCollectionDelegate?
-    
     var session: RiffleAgent?
     var state: String = "Empty"
     var players: [Player] = []
@@ -44,6 +41,10 @@ class GameViewController: UIViewController {
     var app: RiffleAgent!
     var room: RiffleAgent!
     var me: RiffleAgent!
+    
+    
+    // Migrations from the old delegate code
+    var cards: [Card] = []
     
     
     override func viewDidLoad() {
@@ -115,7 +116,7 @@ class GameViewController: UIViewController {
         players.removeObject(player)
         collectionPlayers.reloadData()
     }
-    
+
     func picked(player: Player) {
         
     }
@@ -135,4 +136,171 @@ class GameViewController: UIViewController {
             print("Pick occured outside a valid round! OurChoice: \(currentPlayer.chooser), state: \(state)")
         }
     }
+    
+    //MARK: Interface for interacting with collection objects
+    func set 
+    
+    
+    // MARK: UITableView Delegate and Data Source
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("card") as! CardCell
+        cell.delegate = self
+        cell.labelTitle.text = cards[indexPath.row].text
+        
+        let backView = UIView(frame: cell.frame)
+        backView.backgroundColor = UIColor.clearColor()
+        cell.backgroundView = backView
+        
+        cell.backgroundColor = UIColor.clearColor()
+        cell.backViewbackgroundColor = UIColor.clearColor()
+        cell.contentView.backgroundColor = UIColor.clearColor()
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func swipeTableViewCell(swipeTableViewCell: RMSwipeTableViewCell!, didSwipeToPoint point: CGPoint, velocity: CGPoint) {
+        let cell = swipeTableViewCell as! CardCell
+        
+        let index = table.indexPathForCell(cell)
+        let card = cards[index!.row]
+        
+        // right side selection
+        if point.x >= MAX || point.x <= (-1 * MAX) {
+            // reset the cell
+            cell.resetContentView()
+            cell.interruptPanGestureHandler = true
+            parent.playerSwiped(card)
+        }
+        
+        // Left side selection. Defer for now, although this should represent a "rejection" when choosing
+    }
+    
+    func removeCellsExcept(keep: [Card]) {
+        // removes all cards from the tableview and the table object except those passed
+        
+        var ret: [NSIndexPath] = []
+        
+        for i in 0...(cards.count - 1) {
+            if !keep.contains(cards[i]) {
+                ret.append(NSIndexPath(forRow: i, inSection: 0))
+            }
+        }
+        
+        cards = keep
+        table.deleteRowsAtIndexPaths(ret, withRowAnimation: .Left)
+    }
+
+    
+    //MARK: UICollectionView Delegate and Data Source
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("player", forIndexPath: indexPath) as! PlayerCell
+        let player = players[indexPath.row]
+        
+        cell.labelName.text = player.domain.stringByReplacingOccurrencesOfString("pd.demo.cardsagainst.", withString: "")
+        cell.labelScore.text = "\(player.score)"
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width / 2, height: 100)
+    }
+    
+    func flashCell(target: Player, model: [Player], collection: UICollectionView) {
+        var index = -1
+        
+        for i in 0...model.count {
+            if model[i] == target {
+                index = i
+                break
+            }
+        }
+        
+        // Why does this fail?
+        //    let index = model.indexOf(target)
+        
+        let cell = collection.cellForItemAtIndexPath(NSIndexPath(forRow: index, inSection: 0))
+        UIView.animateWithDuration(0.15, animations: { () -> Void in
+            cell?.backgroundColor = UIColor.whiteColor()
+            }) { (_ :Bool) -> Void in
+                cell?.backgroundColor = UIColor.blackColor()
+        }
+    }
 }
+
+
+// Card cell for the UITableView
+class CardCell: RMSwipeTableViewCell {
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var viewHolder: UIView!
+    
+    func resetContentView() {
+        UIView.animateWithDuration(0.15, animations: { () -> Void in
+            self.contentView.frame = CGRectOffset(self.contentView.bounds, 0.0, 0.0)
+            }) { (b: Bool) -> Void in
+                self.shouldAnimateCellReset = true
+                self.cleanupBackView()
+                self.interruptPanGestureHandler = false
+                self.panner.enabled = true
+        }
+    }
+}
+
+
+// Player cell for Collection View on bottom of screen
+class PlayerCell: UICollectionViewCell {
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelScore: UILabel!
+}
+
+
+// Show progress for the current round with a progress bar
+class TickingView: M13ProgressViewBar {
+    // A simple subclass that ticks down when given a time
+    var timer: NSTimer?
+    var current: Double = 1.0
+    var increment: Double = 0.1
+    let tickRate = 0.1
+    
+    func countdown(time: Double) {
+        if timer != nil {
+            timer!.invalidate()
+            timer = nil
+        }
+        
+        self.primaryColor = UIColor.whiteColor()
+        self.secondaryColor = UIColor.blackColor()
+        
+        increment = tickRate / time
+        current = 1.0
+        self.setProgress(CGFloat(current), animated: true)
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(tickRate, target: self, selector: Selector("tick"), userInfo: nil, repeats: true)
+    }
+    
+    func tick() {
+        current -= increment
+        
+        if current <= 0 {
+            timer?.invalidate()
+            timer = nil
+            current = 1.0
+        } else {
+            self.setProgress(CGFloat(current), animated: true)
+        }
+    }
+}
+
--- ExAgainst/Models.swift
+++ ExAgainst/Models.swift
@@ -42,37 +42,3 @@ func getPlayer(players: [Player], domain: String) -> Player {
     return players.filter({$0.domain == domain})[0]
 }
 
-// Utility function to generate random strings
-func randomStringWithLength (len : Int) -> String {
-    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let randomString : NSMutableString = NSMutableString(capacity: len)
-    
-    for (var i=0; i < len; i++){
-        let rand = arc4random_uniform(UInt32(letters.length))
-        randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
-    }
-    
-    return String(randomString)
-}
-
-// Retrieve a random element from the array and optionally remove it
-func randomElement<T>(inout arr: [T], remove: Bool = false) -> T {
-    let i = Int(arc4random_uniform(UInt32(arr.count)))
-    let o = arr[i]
-    
-    if remove {
-        arr.removeAtIndex(i)
-    }
-    
-    return o
-}
-
-// Remove element by value
-extension RangeReplaceableCollectionType where Generator.Element : Equatable {
-    mutating func removeObject(object : Generator.Element) {
-        if let index = self.indexOf(object) {
-            self.removeAtIndex(index)
-        }
-    }
-}
-
